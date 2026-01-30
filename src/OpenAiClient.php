e837ab9a2168135425b81d32fdf19a882be97765
Mohammed Almassri <mohammed.m.almassri@gmail.com>
2026-01-30 08:47:05 -0600

    Injectable Http Client

--- src/OpenAiClient.php
+++ src/OpenAiClient.php
@@ -4,30 +4,37 @@ declare(strict_types=1);
 
 namespace Massrimcp\OpenAiSdk;
 
-use GuzzleHttp\Client as HttpClient;
+use GuzzleHttp\Client;
 use Massrimcp\OpenAiSdk\V1\Chat\ChatClient;
+use Psr\Http\Client\ClientInterface;
 
 final class OpenAiClient
 {
-    private HttpClient $httpClient;
+    private ClientInterface $httpClient;
 
     public function __construct(
         private readonly string $apiKey,
+        private readonly ?ClientInterface $client = null,
     ) {
+
         if (empty($this->apiKey)) {
             throw new \InvalidArgumentException('api key is required');
         }
-        $this->httpClient = new HttpClient(
-            [
-                'base_uri' => 'https://api.openai.com',
-                'headers' => [
-                    'Accept' => 'application/json',
-                    'Content-Type' => 'application/json',
-                    'Authorization' => "Bearer {$this->apiKey}",
-                ],
-                'timeout' => 60,
-            ]
-        );
+
+        if($client === null) {
+            $this->httpClient = new Client(
+                [
+                    'base_uri' => 'https://api.openai.com',
+                    'headers' => [
+                        'Accept' => 'application/json',
+                        'Content-Type' => 'application/json',
+                        'Authorization' => "Bearer {$this->apiKey}",
+                    ],
+                    'timeout' => 60,
+                ]
+            );
+        }
+
     }
 
     public function chat(): ChatClient
