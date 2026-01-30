ef60bae245effb8cb1b9f1d17325d8fc76b8db31
Mohammed Almassri <mohammed.m.almassri@gmail.com>
2026-01-30 08:34:29 -0600

    ci

--- tests/OpenAiClientTest.php
+++ tests/OpenAiClientTest.php
@@ -1,16 +1,22 @@
 <?php
-declare (strict_types = 1);
 
-namespace Massrimcp\tests;
+declare(strict_types=1);
+
+namespace Massrimcp\OpenAiSdk\Tests;
 
 use Massrimcp\OpenAiSdk\OpenAiClient;
 use PHPUnit\Framework\TestCase;
 
-final class OpenAiSdkTest extends TestCase
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
+final class OpenAiClientTest extends TestCase
 {
     public function testThrowsExceptionOnEmptyApiKey(): void
     {
-        $client = new OpenAiClient("test-key");
-
+        $this->expectException(\InvalidArgumentException::class);
+        $client = new OpenAiClient('');
     }
 }
